34d8bbfcb008751cb3a3f738d9d72b38edcb32b4
osinone <3213709735@qq.com>
2023-07-07 13:44:40 +0800

    init List

---
+++ code/basic/List.h
@@ -0,0 +1,18 @@
+#include "iostream"
+
+class SequentialList
+{
+private:
+    /* data */
+public:
+    SequentialList(/* args */);
+    ~SequentialList();
+};
+
+SequentialList::SequentialList(/* args */)
+{
+}
+
+SequentialList::~SequentialList()
+{
+}
